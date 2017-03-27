2ae664c2d0014172bd2ad6e96ca60a8ac69649eb
vala <gonjo@free.fr>
2017-03-27 11:36:43 +0200

    add role_id getter and setter to active_record extension

--- lib/para/acl/ext/active_record_extension.rb
+++ lib/para/acl/ext/active_record_extension.rb
@@ -7,9 +7,26 @@ module Para
         def has_admin_role
           has_one :user_role, as: :user,
                               class_name: 'Para::Acl::UserRole',
-                              dependent: :destroy
+                              dependent: :destroy,
+                              autosave: true
 
           has_one :role,      through: :user_role
+
+          unless method_defined?(:role_id)
+            define_method(:role_id) do
+              user_role.try(:role_id)
+            end
+          end
+
+          unless method_defined?(:role_id=)
+            define_method(:role_id=) do |id|
+              if user_role
+                user_role.role_id = id
+              else
+                build_user_role(role_id: id)
+              end
+            end
+          end
         end
       end
     end
